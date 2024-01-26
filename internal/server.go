4b0e7849948d2e88be07dc8ed128f6371c0cea4a
toshiyuki.sakai <you@example.com>
2024-01-26 14:27:41 +0900

    added air and cmd/ and internal/server.go

--- internal/server.go
+++ internal/server.go
@@ -1,4 +1,4 @@
-package main
+package internal
 
 import (
 	"net/http"
@@ -6,10 +6,15 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func main() {
+func Run() error {
+
 	e := echo.New()
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
 	e.Logger.Fatal(e.Start(":1323"))
+
+	return nil
 }
