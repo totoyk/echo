6016b3c51c2ebaf8614ebe6a6a5566284572bde4
toshiyuki.sakai <you@example.com>
2024-01-28 17:20:37 +0900

    added gorm for orm and caarlos0/env for config close #10

---
+++ config/env.go
@@ -0,0 +1,22 @@
+package config
+
+import "github.com/caarlos0/env/v10"
+
+type Env struct {
+	// DB
+	DBHost string `env:"DB_HOST,required" envDefault:"db"`
+	DBPort string `env:"DB_PORT,required" envDefault:"13306"`
+	DBUser string `env:"DB_USER,required" envDefault:"root"`
+	DBPass string `env:"DB_PASS,required" envDefault:""`
+	DBName string `env:"DB_NAME,required" envDefault:"pwk"`
+	DBLoc  string `env:"DB_LOC,required" envDefault:"Asia%2FTokyo"`
+}
+
+func LoadEnv() (*Env, error) {
+	var conf Env
+	if err := env.Parse(&conf); err != nil {
+		return nil, err
+	}
+
+	return &conf, nil
+}
---
+++ internal/domain/model/pet.go
@@ -0,0 +1,13 @@
+package model
+
+import (
+	"database/sql"
+
+	"gorm.io/gorm"
+)
+
+type Pet struct {
+	gorm.Model
+	Name string
+	Tag  sql.NullString
+}
